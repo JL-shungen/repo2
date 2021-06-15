70864b39343fcace8e677b54c78ef5e87d56c36b
shungen <1941325247@qq.com>
2021-06-15 11:55:22 +0800

    首次提交项目!!!

---
+++ testGIt/src/package1/testMain.java
@@ -0,0 +1,12 @@
+package package1;
+
+/**
+ * @author 王顺根
+ * @version 1.0
+ * @date 2021/6/15 11:52
+ */
+public class testMain {
+    public static void main(String[] args) {
+        System.out.println("我的名字叫王顺根!");
+    }
+}
